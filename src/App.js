d6950ea009bb088a85d8456856237532906dfdd6
Laura <law7676@outlook.com>
2021-06-15 06:14:34 -0500

    Modified App.js

--- src/App.js
+++ src/App.js
@@ -2,30 +2,10 @@ import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
-  const [emotion, setEmotion] = useState("pissed off");
-  const [secondary, setSecondary] = useState("tired");
-
-useEffect(() => {
-  console.log(`It's ${emotion} around here!`);
-}, [emotion]);
-
-useEffect(() => {
-  console.log(`It's ${secondary} around here!`);
-}, [secondary]);
 
     return (
       <>
-        <h1>Current emotion is {emotion} and {secondary}.</h1>
-        <button onClick={() => setEmotion("happy")}>
-        Happy
-        </button>
-        <button onClick={() => setEmotion("frustrated")}>Frustrate</button>
-        <button onClick={() => setEmotion("enthusiastic")}>
-        Enthuse
-        </button>
-        <button onClick={() => setSecondary("tired")}>
-        Crabby
-        </button>
+        <input type="checkbox" />
       </>
     )
 }
