63c17efb43b8961dc9597d5b26a10fc4bc07d5a6
Laura <law7676@outlook.com>
2021-06-15 06:32:01 -0500

    modified index.js and added useReducer to App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, { useState, useEffect } from "react";
+import React, { useReducer } from "react";
 import './App.css';
 
 function App() {
+  const [checked, toggle] = useReducer(
+    (checked) => !checked,
+    false
+    );
 
-    return (
-      <>
-        <input type="checkbox" />
+  return (
+    <>
+      <input 
+        type="checkbox" 
+        value={checked} 
+        onChange={toggle}
+      />
+      <p>{checked ? "checked" : "not checked"}</p>
       </>
-    )
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-
-const [, , light] = ["boots", "tent", "headlamp"];
-console.log(light);
-
 ReactDOM.render(
     <App />,
   document.getElementById('root')
