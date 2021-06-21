ecbbfb91ebfacc8400d0c8a67ede7dd47489338e
White <whitel1@milwaukee.k12.wi.us>
2021-06-21 10:06:49 -0500

    removed Body.js document\

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import './App.css';
-import Body from './Body';
 
 
 // https://api.github.com/users/l-white
--- src/Body.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Body = (props) => {
-  return (
-    <p>Hello!</p>
-  );
-}
-
-export default Body;
